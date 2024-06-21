5902a7f9c0020b38924f94cd01728b401c860849
Hera222 <Hera222>
2024-06-21 23:37:54 +0300

    first commit

--- src/componets/menu/menu.jsx
+++ src/componets/menu/menu.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./menu.css"; // Archivo CSS donde definiremos los estilos
+import "./menu.css";
 
 function MenuItem({ item }) {
   const [isOpen, setIsOpen] = useState(false);
