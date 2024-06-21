9a2da0a7ff008a81b86c9731e89b2964d20990f0
Hera222 <Hera222>
2024-06-21 21:45:24 +0300

    first commit

--- src/Routes/Estudiantes/Registro.jsx
+++ src/Routes/Estudiantes/Registro.jsx
@@ -11,8 +11,8 @@ export default function Student({ title }) {
   const edit = false;
   const student = [];
   const { HandleNivelClose } = useAppContext();
-  const api = "http://localhost:5000/api/student";
-  //const api = `${hostServer}/api/v3/student`;
+  //const api = "http://localhost:5000/api/student";
+  const api = `${hostServer}/api/v3/student`;
   const [error, setError] = useState(false);
   const initialForm = {
     id: student ? student.id : "",
